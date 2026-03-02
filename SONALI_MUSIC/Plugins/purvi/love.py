51718203cf0d132b886132407106b7618e804fb6
nrecover4587-rgb <nrecover4587@gmail.com>
2026-03-02 18:24:01 +0000

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

---
+++ SONALI_MUSIC/Plugins/purvi/love.py
@@ -0,0 +1,60 @@
+from pyrogram import Client, filters
+import random
+from SONALI_MUSIC import app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def get_random_message(love_percentage):
+    if love_percentage <= 30:
+        return random.choice([
+            "❍ ʟᴏᴠᴇ ɪs ɪɴ ᴛʜᴇ ᴀɪʀ ʙᴜᴛ ɴᴇᴇᴅs ᴀ ʟɪᴛᴛʟᴇ sᴘᴀʀᴋ.",
+            "❍ ᴀ ɢᴏᴏᴅ sᴛᴀʀᴛ ʙᴜᴛ ᴛʜᴇʀᴇ's ʀᴏᴏᴍ ᴛᴏ ɢʀᴏᴡ.",
+            "❍ ɪᴛ's ᴊᴜsᴛ ᴛʜᴇ ʙᴇɢɪɴɴɪɴɢ ᴏғ sᴏᴍᴇᴛʜɪɴɢ ʙᴇᴀᴜᴛɪғᴜʟ."
+        ])
+    elif love_percentage <= 70:
+        return random.choice([
+            "❍ ᴀ sᴛʀᴏɴɢ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ɪs ᴛʜᴇʀᴇ. ᴋᴇᴇᴘ ɴᴜʀᴛᴜʀɪɴɢ ɪᴛ.",
+            "❍ ʏᴏᴜ'ʜᴠ ɢᴏᴛ ᴀ ɢᴏᴏᴅ ᴄʜᴀɴᴄᴇ. ᴡᴏʀᴋ ᴏɴ ɪᴛ.",
+            "❍ ʟᴏᴠᴇ ɪs ʙʟᴏssᴏᴍɪɴɢ, ᴋᴇᴇᴘ ɢᴏɪɴɢ."
+        ])
+    else:
+        return random.choice([
+            "❍ ᴡᴏᴡ ! ɪᴛ's ᴀ ᴍᴀᴛᴄʜ ᴍᴀᴅᴇ ɪɴ ʜᴇᴀᴠᴇɴ!",
+            "❍ ᴘᴇʀғᴇᴄᴛ ᴍᴀᴛᴄʜ ! ᴄʜᴇʀɪsʜ ᴛʜɪs ʙᴏɴᴅ.",
+            "❍ ᴅᴇsᴛɪɴᴇᴅ ᴛᴏ ʙᴇ ᴛᴏɢᴇᴛʜᴇʀ. ᴄᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴs!"
+        ])
+
+
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url="https://t.me/radha_music_Bot?startgroup=true"),
+    ],
+]
+
+
+@app.on_message(filters.command("love"))
+async def love_command(client, message):
+    command, *args = message.text.split(" ")
+    if len(args) >= 2:
+        name1 = args[0].strip()
+        name2 = args[1].strip()
+        
+        love_percentage = random.randint(10, 100)
+        love_message = get_random_message(love_percentage)
+
+        response = (
+            f"❖ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟᴏᴠᴇ ᴘᴇʀᴄᴇɴᴛᴀɢᴇ ⏤͟͟͞͞★ \n\n"
+            f"❅ {name1} ♥️ + {name2} ♥️ = {love_percentage}%\n\n{love_message}"
+        )
+        await client.send_message(
+            message.chat.id,
+            response,
+            reply_markup=InlineKeyboardMarkup(EVAA),
+        )
+    else:
+        await client.send_message(
+            message.chat.id,
+            "❍ ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛᴡᴏ ɴᴀᴍᴇs ᴀғᴛᴇʀ /love ᴄᴏᴍᴍᴀɴᴅ.",
+            reply_markup=InlineKeyboardMarkup(EVAA),
+        )
+            
---
+++ SONALI_MUSIC/help/buttons.py
@@ -0,0 +1,98 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters, enums 
+
+import config
+from SONALI_MUSIC import app
+
+class BUTTONS(object):
+    BBUTTON = [
+        [
+            InlineKeyboardButton("• ᴧɪ | ᴄʜᴧᴛɢᴘᴛ •", callback_data="TOOL_BACK HELP_01"),
+        ],
+        [
+            InlineKeyboardButton("• sєᴧʀᴄʜ •", callback_data="TOOL_BACK HELP_02"),
+            InlineKeyboardButton("• ᴛᴛs •", callback_data="TOOL_BACK HELP_03"),
+            InlineKeyboardButton("• ɪηғσ •", callback_data="TOOL_BACK HELP_04"),
+        ],
+        [
+            InlineKeyboardButton("• ғσηᴛ •", callback_data="TOOL_BACK HELP_05"),
+            InlineKeyboardButton("• ϻᴧᴛʜ •", callback_data="TOOL_BACK HELP_06"),
+            InlineKeyboardButton("• ᴛᴧɢᴧʟʟ •", callback_data="TOOL_BACK HELP_07"),
+        ],
+        [
+            InlineKeyboardButton("• ɪϻᴧɢє •", callback_data="TOOL_BACK HELP_08"),
+            InlineKeyboardButton("• ʜᴧsᴛᴧɢ •", callback_data="TOOL_BACK HELP_09"),
+            InlineKeyboardButton("• sᴛɪᴄᴋєʀs •", callback_data="TOOL_BACK HELP_10"),
+        ],
+        [
+            InlineKeyboardButton("• ғυη •", callback_data="TOOL_BACK HELP_11"),
+            InlineKeyboardButton("• ǫυσᴛʟʏ •", callback_data="TOOL_BACK HELP_12"),
+            InlineKeyboardButton("• ᴛ-ᴅ •", callback_data="TOOL_BACK HELP_13"),
+        ],
+        [   
+            InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"MAIN_CP"),]
+        ]
+    
+    MBUTTON = [
+                [
+            InlineKeyboardButton("• єxᴛʀᴧ •", callback_data="MANAGEMENT_BACK HELP_25"),
+        ],
+        [
+            InlineKeyboardButton("• ʙᴧη •", callback_data="MANAGEMENT_BACK HELP_14"),
+            InlineKeyboardButton("• ᴋɪᴄᴋ •", callback_data="MANAGEMENT_BACK HELP_15"),
+            InlineKeyboardButton("• ϻυᴛє •", callback_data="MANAGEMENT_BACK HELP_16"),
+        ],
+        [
+            InlineKeyboardButton("• ᴘɪη •", callback_data="MANAGEMENT_BACK HELP_17"),
+            InlineKeyboardButton("• sᴛᴧғғ •", callback_data="MANAGEMENT_BACK HELP_18"),
+            InlineKeyboardButton("• sєᴛ-υᴘ •", callback_data="MANAGEMENT_BACK HELP_19"),
+        ],
+        [
+            InlineKeyboardButton("• ᴢσϻʙɪє •", callback_data="MANAGEMENT_BACK HELP_20"),
+            InlineKeyboardButton("• ɢᴧϻє •", callback_data="MANAGEMENT_BACK HELP_21"),
+            InlineKeyboardButton("• ɪϻᴘσsᴛєʀ •", callback_data="MANAGEMENT_BACK HELP_22"),
+        ],
+        [
+            InlineKeyboardButton("• sɢ •", callback_data="MANAGEMENT_BACK HELP_23"),
+            InlineKeyboardButton("• ᴛʀ •", callback_data="MANAGEMENT_BACK HELP_24"),
+            InlineKeyboardButton("• ɢʀᴧᴘʜ •", callback_data="MANAGEMENT_BACK HELP_26"),
+        ],
+        [
+            InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"MAIN_CP"), 
+        ]
+        ]
+    PBUTTON = [
+        [
+            InlineKeyboardButton("˹ ᴅᴇᴠ ˼", url="https://t.me/icxasta")
+        ],
+        [
+            InlineKeyboardButton("⌯ ʙᴧᴄᴋ ᴛσ ʜσϻє ⌯", callback_data="settingsback_helper"),
+            
+        ]
+        ]
+    
+    ABUTTON = [
+        [
+            InlineKeyboardButton("˹ sυᴘᴘσʀᴛ ˼", url="https://t.me/odsnetwork"),
+            InlineKeyboardButton("˹ υᴘᴅᴧᴛєs ˼", url="https://t.me/ixasta1"),
+        ],
+        [
+            InlineKeyboardButton("⌯ ʙᴧᴄᴋ ᴛσ ʜσϻє ⌯", callback_data="settingsback_helper"),
+            
+        ]
+        ]
+    
+    SBUTTON = [
+        [
+            InlineKeyboardButton("ϻυsɪᴄ", callback_data="settings_back_helper"),
+            InlineKeyboardButton("ϻᴧηᴧɢєϻєηᴛ", callback_data="MANAGEMENT_CP"),
+        ],
+        [
+            InlineKeyboardButton("ᴛσσʟs", callback_data="TOOL_CP"),
+            InlineKeyboardButton("ᴧʙσυт", callback_data="MAIN_BACK HELP_ABOUT"),
+        ],
+        [
+            InlineKeyboardButton("⌯ ʙᴧᴄᴋ ᴛσ ʜσϻє ⌯", callback_data="settingsback_helper"),
+            
+        ]
+    ]
