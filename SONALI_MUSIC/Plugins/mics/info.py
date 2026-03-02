3a94dbc534f6fadfa9a1ce6cc7927b2859cb177d
nrecover4587-rgb <nrecover4587@gmail.com>
2026-03-03 00:56:42 +0530

    Update info.py

--- SONALI_MUSIC/Plugins/mics/info.py
+++ SONALI_MUSIC/Plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Laibaamusicbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/MafiyamusicRobot?startgroup=true"),
     ],
 ]
 
